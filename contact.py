0bbb85efeba9c1eca4c1ae187065fc2f0a634bd2
chikos96 <tchikos741@gmail.com>
2021-02-04 09:59:18 +0000

    Add Session User Profile

--- contact.py
+++ contact.py
@@ -74,6 +74,18 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
