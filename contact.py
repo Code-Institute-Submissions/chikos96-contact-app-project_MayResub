b400d94376000f6844b76c8814737cbc031e975f
chikos96 <tchikos741@gmail.com>
2021-02-15 10:38:25 +0000

    Update contact.py

--- contact.py
+++ contact.py
@@ -127,4 +127,4 @@ def delete_contact(contact_id):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")), debug=True)
+            port=int(os.environ.get("PORT")), debug=False)
